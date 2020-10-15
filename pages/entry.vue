3fa6a83773d4c75d94095d7c23a16a6caef363ac
Danil Korennykh <bluskript@gmail.com>
2020-10-15 15:21:38 -0400

    niceify entry menu

--- pages/entry.vue
+++ pages/entry.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="root">
-    <v-sheet class="body pa-6" rounded>
-      <nuxt-child />
-    </v-sheet>
-  </div>
+  <fragment>
+    <v-app-bar fixed color="transparent" flat>
+      <v-toolbar-title>Staccato</v-toolbar-title>
+      <v-spacer />
+      <v-btn text>
+        <v-icon> mdi-translate </v-icon>
+      </v-btn>
+    </v-app-bar>
+    <div class="root">
+      <v-sheet class="body pa-6" rounded>
+        <nuxt-child />
+      </v-sheet>
+    </div>
+  </fragment>
 </template>
 
 <style scoped>
@@ -12,17 +21,31 @@
   height: 100%;
   display: flex;
   align-items: center;
-  justify-content: center;
+  justify-content: left;
+  background: url('~@/static/wiggle.svg');
+  background-repeat: repeat;
+  stroke: black;
 }
 .body {
-  width: 60%;
+  width: 30%;
+  margin-left: 5%;
+}
+
+@media only screen and (max-width: 900px) {
+  .body {
+    width: 90%;
+  }
 }
 </style>
 
 <script lang="ts">
 import Vue from 'vue'
+import { Fragment } from 'vue-fragment'
 
 export default Vue.extend({
+  components: {
+    Fragment,
+  },
   computed: {
     step() {
       return this.$accessor.entry.step
--- pages/entry/auth.vue
+++ pages/entry/auth.vue
@@ -1,11 +1,19 @@
 <template>
   <div>
-    <v-btn />
+    <h3 class="mb-4">Login To {{ host }}</h3>
+    <v-text-field label="Email" outlined type="email"></v-text-field>
+    <v-text-field label="Password" outlined type="password"></v-text-field>
   </div>
 </template>
 
-<script>
+<script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({})
+export default Vue.extend({
+  computed: {
+    host() {
+      return this.$route.hash.substr(1)
+    },
+  },
+})
 </script>
