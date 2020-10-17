5a689b14077b1d35e4ae0c31f69b718298fe2077
Danil Korennykh <bluskript@gmail.com>
2020-10-17 17:49:56 -0400

    improve guild dialog UI

---
+++ components/GuildList/GuildMenu/JoinGuild.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-card flat>
+    <v-card-text>
+      <v-text-field outlined label="Invite Link" hide-details="auto">
+        <template v-slot:append>
+          <v-tooltip bottom>
+            <template v-slot:activator="{ on }">
+              <v-icon v-on="on"> mdi-help-circle-outline </v-icon>
+            </template>
+            Invites look like this: <br />
+            asdf72 <br />
+            harmony://[domain]/[invite] <br />
+            epic-discord
+          </v-tooltip>
+        </template>
+      </v-text-field>
+      <div class="mt-3">
+        <v-btn color="primary">Join Guild</v-btn>
+        <v-btn text>Cancel</v-btn>
+      </div>
+    </v-card-text>
+  </v-card>
+</template>
--- components/GuildList/GuildMenuButton.vue
+++ components/GuildList/GuildMenuButton.vue
@@ -10,28 +10,7 @@
         <v-tab>Join Guild</v-tab>
         <v-tab>Create Guild</v-tab>
         <v-tab-item>
-          <v-card flat>
-            <v-card-text>
-              <v-text-field outlined label="Invite Link" hide-details="auto">
-                <template v-slot:append>
-                  <v-tooltip bottom>
-                    <template v-slot:activator="{ on }">
-                      <v-icon v-on="on"> mdi-help-circle-outline </v-icon>
-                    </template>
-                    Invites look like this: <br />
-                    asdf72 <br />
-                    harmony://[domain]/[invite] <br />
-                    epic-discord
-                  </v-tooltip>
-                </template>
-              </v-text-field>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn text>Cancel</v-btn>
-              <v-spacer />
-              <v-btn color="primary">Join Guild</v-btn>
-            </v-card-actions>
-          </v-card>
+          <join-guild-menu />
         </v-tab-item>
         <v-tab-item>
           <v-card flat> </v-card>
@@ -43,7 +22,12 @@
 
 <script lang="ts">
 import Vue from 'vue'
+import JoinGuildMenu from './GuildMenu/JoinGuild.vue'
+
 export default Vue.extend({
+  components: {
+    JoinGuildMenu,
+  },
   data() {
     return {
       dialogOpen: false,
