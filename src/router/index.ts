756b8a708b1d9379e8fb77015dc5173237c13a30
Danil Korennykh <bluskript@gmail.com>
2020-10-22 13:38:21 -0400

    remove some boilerplate

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,18 @@
 import Vue from "vue";
 import VueRouter, { RouteConfig } from "vue-router";
-import Home from "../views/Home.vue";
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: "/",
-    name: "Home",
-    component: Home
+    redirect: '/entry/serverselect'
   },
   {
     path: "/about",
     name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue")
+      import("../views/About.vue")
   }
 ];
 
