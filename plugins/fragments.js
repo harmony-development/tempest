e79be85f26532da30b401548e734857872721d91
Danil Korennykh <bluskript@gmail.com>
2020-10-22 13:03:58 -0400

    erase project

--- plugins/fragments.js
+++
@@ -1,4 +0,0 @@
-import Vue from 'vue'
-import { Fragment } from 'vue-fragment'
-
-Vue.component('fragment', Fragment)
--- store/index.ts
+++
@@ -1,8 +0,0 @@
-import { getAccessorType } from 'nuxt-typed-vuex'
-import * as entry from './entry'
-import * as app from './app'
-import * as dialog from './dialog'
-
-export const accessorType = getAccessorType({
-  modules: { entry, app, dialog },
-})
