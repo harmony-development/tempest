00536fce4d00412a3c71d091090944bbfdc5634b
Danil Korennykh <bluskript@gmail.com>
2021-01-13 20:26:24 -0500

    error boundary

---
+++ components/ErrorBoundary.vue
@@ -0,0 +1,20 @@
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'ErrorBoundary',
+  data: () => ({
+    error: false,
+    description: undefined as string | undefined,
+  }),
+  errorCaptured(err, _vm, _info) {
+    this.error = true
+    this.description = err.message
+  },
+  render(_h) {
+    if (this.error) {
+      return this.$slots.error![0]
+    }
+    return this.$slots.default![0]
+  },
+})
+</script>
