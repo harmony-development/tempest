3146160a8af077217fd90973864de349be551422
Danil Korennykh <bluskript@gmail.com>
2021-01-17 21:35:39 -0500

    fix(app): only include icons that are in use

--- plugins/fontawesome.ts
+++ plugins/fontawesome.ts
@@ -2,10 +2,18 @@ import Vue from 'vue'
 
 import { library, config } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { fas } from '@fortawesome/free-solid-svg-icons'
+import {
+  faChevronRight,
+  faGlobe,
+  faSpinner,
+  faTrash,
+} from '@fortawesome/free-solid-svg-icons'
 
 config.autoAddCss = false
 
-library.add(fas)
+library.add(faSpinner)
+library.add(faTrash)
+library.add(faChevronRight)
+library.add(faGlobe)
 
 Vue.component('fa-icon', FontAwesomeIcon)
