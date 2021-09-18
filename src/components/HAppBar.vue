ae8c6a6274fef31f56c9f80e38511ec52e7b7531
Bluskript <bluskript@gmail.com>
2021-09-18 18:14:56 -0400

    feat: remove windi hack

--- src/components/HAppBar.vue
+++ src/components/HAppBar.vue
@@ -4,15 +4,10 @@ import { defineProps } from "vue";
 const { absolute } = defineProps({
   absolute: Boolean,
 });
-
-const classes = {
-  appbar: true,
-  absolute,
-};
 </script>
 
 <template>
-  <div :class="classes">
+  <div class="appbar" :class="{ absolute }">
     <slot />
   </div>
 </template>
@@ -20,10 +15,5 @@ const classes = {
 <style lang="postcss" scoped>
 .appbar {
   @apply w-full bg-gray-100 dark:bg-harmonydark-600 mx-auto p-3 flex items-center h-14;
-}
-
-/* TODO: REMOVE THIS WHEN https://github.com/windicss/vue-windicss-preprocess/issues/14 IS RESOLVED */
-.absolute {
-  @apply absolute;
 }
 </style>
