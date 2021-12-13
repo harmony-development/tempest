bc500d1ec23f80cbd9cabb36230167fe43b23e1f
Bluskript <bluskript@gmail.com>
2021-12-12 19:34:21 -0500

    avatars yayyyyyy

--- src/components/chat/Avatar.vue
+++ src/components/chat/Avatar.vue
@@ -3,6 +3,7 @@ import { computed } from "vue";
 import { chatState } from "../../logic/store/chat";
 import HImg from "../shared/HImg.vue";
 import { useChatRoute } from "../../router";
+import { parseHMC } from '../../logic/parsing';
 
 const { host } = useChatRoute();
 
@@ -13,12 +14,13 @@ const profile = computed(
   () => chatState.getUser(host.value!, props.userid),
   undefined
 );
+const uri = computed(() => profile.value?.picture && host.value && parseHMC(profile.value?.picture, host.value));
 </script>
 
 <template>
   <HImg
     :fallback="profile?.username[0] || userid[0]"
-    :src="profile?.picture"
-    class="bg-primary-800 hover:bg-primary-900 p-2 rounded-xl square"
+    :src="uri"
+    class="bg-primary-800 hover:bg-primary-900 rounded-xl square"
   />
 </template>
