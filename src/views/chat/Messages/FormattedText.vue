4cbdea70f3a6dff71828c86a59b90d6445483781
Bluskript <bluskript@gmail.com>
2022-02-15 07:16:13 -0500

    fix import

--- src/views/chat/Messages/FormattedText.vue
+++ src/views/chat/Messages/FormattedText.vue
@@ -1,7 +1,6 @@
 <script lang="ts" setup>
-import type { FormattedText } from "@harmony-dev/harmony-web-sdk/dist/gen/chat/v1/messages";
+import type { Format } from "@harmony-dev/harmony-web-sdk/dist/gen/chat/v1/messages";
 import { computed } from "vue";
-import type { Format } from "../../../../../harmony-web-sdk/gen/chat/v1/messages";
 import { linkify } from "~/logic/formatting";
 
 const props = defineProps<{
